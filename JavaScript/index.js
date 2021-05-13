5f56cdab624888fc5a8ea1113fe9376f148eb86b
DiBDV <bdv.dmitry@gmail.com>
2021-05-13 23:54:32 +0200

    adding gitignre, updated index.js, installed lodash

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,2 +1,6 @@
+// По общепринятому соглашению, lodash импортируется под именем _
+import _ from "./node_modules/lodash/lodash.js";
+
 console.log("Hello, Hexlet!");
 
+console.log(_.last(['one', 'two']));
